1266cbf02c9e1e30fdbf0ea228ffbd7d6b3e2806
Filo44 <Filo__@outlook.com>
2023-06-11 13:59:10 +0100

    WORKS REALLY WELL

--- mazeGeneratingAlgo.js
+++ mazeGeneratingAlgo.js
@@ -2,11 +2,12 @@ let grid = []
 let m = 5
 let n = 5
 const dirArray = ["top", "right", "bottom", "left"]
-
 //I is for rows
 //J is for columns
+var min = Infinity
 
 for (let i = 0; i < m; i++) {
+    grid[i] = []
     for (let j = 0; j < n; j++) {
         grid[i][j] = { top: true, right: true, bottom: true, left: true, num: (i * n) + j }
         if (j == 0) {
@@ -29,35 +30,96 @@ while (true) {
     let currentJ = ranCellNum % n
     let cellCopy = grid[currentI][currentJ]
 
-    let plausibleChoices = []
-    Object.keys(cellCopy).forEach((key) => {
-        if (key != "num" && cellCopy[key] == true) {
-            plausibleChoices.push(key)
-        }
-    })
+    // let plausibleChoices = []
+    // Object.keys(cellCopy).forEach((key) => {
+    //     if (key != "num" && cellCopy[key] == true) {
+    //         plausibleChoices.push(key)
+    //     }
+    // })
+    let plausibleChoices = openWalls(cellCopy)
+
     if (plausibleChoices.length != 0) {
         let wallChoice = plausibleChoices[Math.floor(Math.random() * plausibleChoices.length)]
         cellCopy[wallChoice] = false
         let wallAdjCell = { complete: null, direction: null }
+        console.log(plausibleChoices)
+        console.log(wallChoice)
+        console.log(grid)
         if (wallChoice == "top") {
-            cellCopy.num = Math.min(cellCopy.num, grid[currentI - 1][currentJ].num)
             wallAdjCell.complete = grid[currentI - 1][currentJ]
-            direction = "bottom"
-            grid[currentI - 1][currentJ].bottom = false
+            wallAdjCell.direction = "bottom"
+            console.log(wallChoice)
         } else if (wallChoice == "right") {
-            wallAdjCell.complete = grid[currentI][currentJ - 1]
-            direction = "left"
+            wallAdjCell.complete = grid[currentI][currentJ + 1]
+            wallAdjCell.direction = "left"
+            console.log(wallChoice)
         } else if (wallChoice == "bottom") {
             wallAdjCell.complete = grid[currentI + 1][currentJ]
-            direction = "top"
+            wallAdjCell.direction = "top"
+            console.log(wallChoice)
         } else if (wallChoice == "left") {
-            wallAdjCell.complete = grid[currentI][currentJ + 1]
-            direction = "right"
+            wallAdjCell.complete = grid[currentI][currentJ - 1]
+            wallAdjCell.direction = "right"
+            console.log(wallChoice)
         }
+        console.log(wallAdjCell)
         let smallNum = Math.min(cellCopy.num, wallAdjCell.complete.num)
+        let bigNum = Math.max(cellCopy.num, wallAdjCell.complete.num)
+        //*Could do recursion instead of just brute force for optimization
+
         cellCopy.num = smallNum
         wallAdjCell.complete.num = smallNum
         wallAdjCell.complete[wallAdjCell.direction] = false
+        // grid = changeValueFromValue(bigNum, smallNum, grid)
+        minValue(currentI, currentJ, "asldf")
     }
     //Make it choose a cell from a list where there are only tiles where there 
+}
+
+function changeValueFromValue(searchValue, instertValue, grid) {
+    let res = []
+    for (let i = 0; i < grid.length; i++) {
+        res[i] = []
+        for (let j = 0; j < grid[i].length; j++) {
+            if (grid[i][j].num == searchValue) {
+                res[i][j] = { ...grid[i][j], num: instertValue }
+            } else {
+                res[i][j] = grid[i][j]
+            }
+        }
+    }
+    console.log(res)
+    return res
+}
+// function recursiveValueChange(i, j, wall) {
+//     min = Infinity
+// }
+function minValue(i, j, cameFromWall) {
+    min = Math.min(grid[i][j].num, min)
+    let openWallsArr = openWalls(grid[i][j])
+    openWallsArr = openWallsArr.filter(val => {
+        return val != cameFromWall
+    })
+    if (openWalls.length > 0) {
+        openWalls(grid[i][j]).forEach((wall) => {
+            if (wall == "top") {
+                minValue(i - 1, j)
+            } else if (wall == "right") {
+                minValue(i, j + 1)
+            } else if (wall == "bottom") {
+                minValue(i + 1, j)
+            } else if (wall == "left") {
+                minValue(i, j - 1)
+            }
+        })
+    }
+}
+function openWalls(cell) {
+    let plausibleChoices = []
+    Object.keys(cell).forEach((key) => {
+        if (key != "num" && cell[key] == true) {
+            plausibleChoices.push(key)
+        }
+    })
+    return plausibleChoices
 }
